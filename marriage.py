38cd9e10f15e1faa3358f3295c0c71bb1c0873de
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-01-29 00:37:42 -0500

    able to read from files and split the lines into arrays

--- marriage.py
+++ marriage.py
@@ -1,14 +1,20 @@
 import sys
 
+def add_argument(argument=1):
+    if len(sys.argv) > 2 or len(sys.argv) == 1:
+        sys.stdout.write("nothing")
+        exit(1)
+    else:
+        return sys.argv[argument]
 
-try:
-    file_name = sys.argv[1]
+def open_file():
+    file_name = add_argument()
     with open(file_name) as file:
         data = file.readlines()
         for line in data:
             words = line.split()
             print(words)
-            #if len(line.split) ==
-except:
-    sys.stdout.write("nothing")
-    exit(1)
+
+if __name__ == "__main__":
+    add_argument()
+    open_file()

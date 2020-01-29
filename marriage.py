5697705c5bce6af1b5dd4a8791a9796cdb072803
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-01-28 22:53:16 -0500

    splited each line of file into arrays

--- marriage.py
+++ marriage.py
@@ -1,11 +1,14 @@
 import sys
 
-#print(sys.argv)
+
 try:
     file_name = sys.argv[1]
     with open(file_name) as file:
-        data = file.read()
-        print(data)
+        data = file.readlines()
+        for line in data:
+            words = line.split()
+            print(words)
+            #if len(line.split) ==
 except:
     sys.stdout.write("nothing")
     exit(1)

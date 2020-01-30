b9b5ab55240e4b1353f727de0472020d76b0764a
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-01-29 22:29:54 -0500

    added a except handling function

--- marriage.py
+++ marriage.py
@@ -1,22 +1,43 @@
 import sys
 
-def add_argument(argument=1):
+#len(men) and len(women)
+N = 0
+file_rows = []
+
+#men = {}
+#women = {}
+
+def addArgument(argument=1):
     if len(sys.argv) > 2 or len(sys.argv) == 1:
         sys.stdout.write("nothing")
         sys.exit(1)
     else:
         return sys.argv[argument]
 
-def open_file():
-    file_name = add_argument()
+def openFile():
+    global N
+    file_name = addArgument()
     with open(file_name) as file:
         data = file.readlines()
+        N = int(data[0])
         for line in data:
             words = line.split()
+            file_rows.append(words)
+        file_rows.pop(0)
+        return file_rows
 
-        size = data[0]
-        print(size)
-        return print(words[1:])
+def checker():
+    global N
+    rows = openFile()
+    for row in rows:
+        if len(row) == N+1:
+            continue
+        else:
+            sys.stdout.write('nothing')
+            sys.exit(1)
+    # print(i)
+
 if __name__ == "__main__":
-    add_argument()
-    open_file()
+    addArgument()
+    #openFile()
+    checker()

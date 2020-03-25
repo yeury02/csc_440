94a15a56f59b5087ef8a1b03bb0bc57a57eed029
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-03-24 20:09:04 -0700

    preparing for assignment 4

--- ass3/rubik/solver.py
+++ ass3/rubik/solver.py
@@ -5,7 +5,41 @@ import time
 '''
 Algorithm Invarient
 
-Definition: 
+Definition: 2x2x2 Rubik's Cube, the goal is to make make each side the same color. 
+            I accomplished this by using a Breath First Search(BFS) traversal algorithm 
+            going two ways. The Start of the cube and the end which are given to us as a
+            tuple. Let G=(V,E) where V is the a set of vertices and E is a set of edges (pair of vertices)
+            Let start = S and end = E
+
+Initialization (Base Case): I know this base case is working because my program checks if the given S set
+                            of points is equal to the E set of points, then the 2x2x2 cube most be solved already.
+                            It is technically checking if the moves are in the same order. If cubes are not on the 
+                            same state, then I use a two way breath first search algorithm to solve it.
+
+Maintance (Indunction Step) - Assuming there is a path from S to E and they are not on the same state, 
+                              start with vertex V
+                              list all its neighbors(distance 1)
+                              then all their neighbors (distance 2) etc.
+                              
+                              Algorithm starting at S:
+                                   define frontier F
+                                   initially = F{s}
+                                   repeat F=all neighbors of vertices in F
+                                   until all vertices found
+                                
+                              Algorithm starting at S:
+                                   define frontier F
+                                   initially = F{e}
+                                   repeat F=all neighbors of vertices in F
+                                   until all vertices found  
+                                
+                              as mentioned above, this is a two way searching, I would first make my first move
+                              with S and see if there is a path to E, if not then make a move with E and check
+                              the same thing over and over.
+    
+    termination - I know this algorithm terminates correctly because if it finds a path from S to E then it returns
+                  that part. However, it keeps checking for paths until it runs out of possible ways to check. If
+                  that happens then that means there is not a solution.                   
 
 '''
 def shortest_path(start, end):
---
+++ ass4/huffman.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import marshal
+import array
+
+try:
+    import cPickle as pickle
+except:
+    import pickle
+
+def encode(msg):
+
+    raise NotImplementedError
+
+def decode(msg, decoderRing):
+
+    raise NotImplementedError
+
+def compress(msg):
+
+    # Initializes an array to hold the compressed message.
+    compressed = array.array('B')
+    raise NotImplementedError
+
+def decompress(msg, decoderRing):
+
+    # Represent the message as an array
+    byteArray = array.array('B',msg)
+    raise NotImplementedError
+
+def usage():
+    sys.stderr.write("Usage: {} [-c|-d|-v|-w] infile outfile\n".format(sys.argv[0]))
+    exit(1)
+
+if __name__=='__main__':
+    if len(sys.argv) != 4:
+        usage()
+    opt = sys.argv[1]
+    compressing = False
+    decompressing = False
+    encoding = False
+    decoding = False
+    if opt == "-c":
+        compressing = True
+    elif opt == "-d":
+        decompressing = True
+    elif opt == "-v":
+        encoding = True
+    elif opt == "-w":
+        decoding = True
+    else:
+        usage()
+
+    infile = sys.argv[2]
+    outfile = sys.argv[3]
+    assert os.path.exists(infile)
+
+    if compressing or encoding:
+        fp = open(infile, 'rb')
+        msg = fp.read()
+        fp.close()
+        if compressing:
+            compr, decoder = compress(msg)
+            fcompressed = open(outfile, 'wb')
+            marshal.dump((pickle.dumps(decoder), compr), fcompressed)
+            fcompressed.close()
+        else:
+            enc, decoder = encode(msg)
+            print(msg)
+            fcompressed = open(outfile, 'wb')
+            marshal.dump((pickle.dumps(decoder), enc), fcompressed)
+            fcompressed.close()
+    else:
+        fp = open(infile, 'rb')
+        pickleRick, compr = marshal.load(fp)
+        decoder = pickle.loads(pickleRick)
+        fp.close()
+        if decompressing:
+            msg = decompress(compr, decoder)
+        else:
+            msg = decode(compr, decoder)
+        fp = open(outfile, 'wb')
+        fp.write(msg)
+        fp.close()

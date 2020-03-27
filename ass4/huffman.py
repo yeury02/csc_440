c0fd736c6e22a0399493bdb0447f30cfde6f8e48
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-03-26 22:11:18 -0700

    working on building a tree

--- ass4/huffman.py
+++ ass4/huffman.py
@@ -2,7 +2,7 @@ import os
 import sys
 import marshal
 import array
-from heapq import heappush, heappop, heapify
+import heapq
 
 try:
     import cPickle as pickle
@@ -20,17 +20,32 @@ def encode(msg):
     tuples = []
     for char in letters :
         tuples.append((frequency[char],char))
-        tuples.sort()
+    tuples.sort()
     return tuples
+
+def build_tree(tuples):
+    heap = []
+    for l_f in tuples: heapq.heappush(heap, [l_f])
+    # print(heap)
+    while (len(heap) > 1):
+        left_child = heapq.heappop(heap)
+        right_child = heapq.heappop(heap)
+        #print(left_child, right_child)
+        left_freq, left_char = left_child[0]
+        right_freq, right_char = right_child[0]
+        print(left_freq, left_char)
+        print(right_freq, right_char)
+
     
-def buildTree(tuples):
-    while len(tuples) > 1 :
-        leastTwo = tuple(tuples[0:2])                  # get the 2 to combine
-        theRest  = tuples[2:]                          # all the others
-        combFreq = leastTwo[0][0] + leastTwo[1][0]     # the branch points freq
-        tuples   = theRest + [(combFreq,leastTwo)]     # add branch point to the end
-        tuples.sort()                                  # sort it into place
-    return tuples[0]                                   # Return the single tree inside the list
+# def buildTree():
+#     tuples = encode(msg)
+#     while len(tuples) > 1 :
+#         leastTwo = tuple(tuples[0:2])                  # get the 2 to combine
+#         theRest  = tuples[2:]                          # all the others
+#         combFreq = leastTwo[0][0] + leastTwo[1][0]     # the branch points freq
+#         tuples   = theRest + [(combFreq,leastTwo)]     # add branch point to the end
+#         tuples.sort()                                  # sort it into place
+#     return tuples[0]                                   # Return the single tree inside the list
 
 
 
@@ -71,53 +86,57 @@ def usage():
 
 if __name__=='__main__':
     msg = 'hello'
-    print(encode(msg))
-    if len(sys.argv) != 4:
-        usage()
-    opt = sys.argv[1]
-    compressing = False
-    decompressing = False
-    encoding = False
-    decoding = False
-    if opt == "-c":
-        compressing = True
-    elif opt == "-d":
-        decompressing = True
-    elif opt == "-v":
-        encoding = True
-    elif opt == "-w":
-        decoding = True
-    else:
-        usage()
+    tuples = encode(msg)
+    build_tree(tuples)
+    # print(buildTree())
 
-    infile = sys.argv[2]
-    outfile = sys.argv[3]
-    assert os.path.exists(infile)
 
-    if compressing or encoding:
-        fp = open(infile, 'rb')
-        msg = fp.read()
-        fp.close()
-        if compressing:
-            compr, decoder = compress(msg)
-            fcompressed = open(outfile, 'wb')
-            marshal.dump((pickle.dumps(decoder), compr), fcompressed)
-            fcompressed.close()
-        else:
-            enc, decoder = encode(msg)
-            print(msg)
-            fcompressed = open(outfile, 'wb')
-            marshal.dump((pickle.dumps(decoder), enc), fcompressed)
-            fcompressed.close()
-    else:
-        fp = open(infile, 'rb')
-        pickleRick, compr = marshal.load(fp)
-        decoder = pickle.loads(pickleRick)
-        fp.close()
-        if decompressing:
-            msg = decompress(compr, decoder)
-        else:
-            msg = decode(compr, decoder)
-        fp = open(outfile, 'wb')
-        fp.write(msg)
-        fp.close()
+    # if len(sys.argv) != 4:
+    #     usage()
+    # opt = sys.argv[1]
+    # compressing = False
+    # decompressing = False
+    # encoding = False
+    # decoding = False
+    # if opt == "-c":
+    #     compressing = True
+    # elif opt == "-d":
+    #     decompressing = True
+    # elif opt == "-v":
+    #     encoding = True
+    # elif opt == "-w":
+    #     decoding = True
+    # else:
+    #     usage()
+
+    # infile = sys.argv[2]
+    # outfile = sys.argv[3]
+    # assert os.path.exists(infile)
+
+    # if compressing or encoding:
+    #     fp = open(infile, 'rb')
+    #     msg = fp.read()
+    #     fp.close()
+    #     if compressing:
+    #         compr, decoder = compress(msg)
+    #         fcompressed = open(outfile, 'wb')
+    #         marshal.dump((pickle.dumps(decoder), compr), fcompressed)
+    #         fcompressed.close()
+    #     else:
+    #         enc, decoder = encode(msg)
+    #         print(msg)
+    #         fcompressed = open(outfile, 'wb')
+    #         marshal.dump((pickle.dumps(decoder), enc), fcompressed)
+    #         fcompressed.close()
+    # else:
+    #     fp = open(infile, 'rb')
+    #     pickleRick, compr = marshal.load(fp)
+    #     decoder = pickle.loads(pickleRick)
+    #     fp.close()
+    #     if decompressing:
+    #         msg = decompress(compr, decoder)
+    #     else:
+    #         msg = decode(compr, decoder)
+    #     fp = open(outfile, 'wb')
+    #     fp.write(msg)
+    #     fp.close()

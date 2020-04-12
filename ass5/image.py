a44a1c18226fde970a08ed61ba8e50e0079ca294
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-04-12 16:01:57 -0700

    working

---
+++ ass5/image.py
@@ -0,0 +1,68 @@
+import imagematrix
+
+class ResizeableImage(imagematrix.ImageMatrix):
+
+    def naive_path(self, w, h, path, e):
+        if h == self.height - 1:
+            self.seams.append((path,e))
+            return path
+
+        for c in range(max(0, w - 1), min(self.width, w + 2)):
+            energy = self.energy(c, (h + 1))
+            pixel = [(c, h + 1)]
+            self.naive_path(c, h + 1, path + pixel, e + energy)
+        
+        return
+
+    def dynamic_path(self, matrix, tracker):
+        for w in range(1, self.height):
+            for h in range(self.width):
+                if h == 0:
+                    neighbors = [matrix[w - 1][h], matrix[w - 1][h + 1]]
+                    mini = neighbors.index(min(neighbors))
+                    tracker[w][h] = mini + h
+                    mine = matrix[w - 1][mini + h]
+                elif h == self.width - 1:
+                    neighbors = [matrix[w - 1][h - 1], matrix[w - 1][h]] 
+                    mini = neighbors.index(min(neighbors))
+                    tracker[w][h] = mini + h - 1
+                    mine = matrix[w - 1][mini + h - 1]
+                else:
+                    neighbors = [matrix[w - 1][h - 1], matrix[w - 1][h], matrix[w - 1][h + 1]] 
+                    mini = neighbors.index(min(neighbors))
+                    tracker[w][h] = mini + h - 1
+                    mine = matrix[w - 1][mini + h - 1]
+                matrix[w][h] = matrix[w][h] + mine
+        row = matrix[-1]
+        lowest_sum = min(row)
+        column = row.index(min(row))
+        path = []
+        for w in reversed(range(self.height)):
+            path = [(column, w)] + path
+            column = tracker[w][column]
+        return (path, lowest_sum)    
+                    
+    def best_seam(self, dp=True):
+        if dp:
+            energy_matrix = []
+            tracker = []
+            for h in range(self.height):
+                rowe = []
+                rowt = []
+                for w in range(self.width):
+                    rowe.append(self.energy(w, h))
+                    rowt.append(0)
+                energy_matrix.append(rowe)
+                tracker.append(rowt)
+            best = self.dynamic_path(energy_matrix, tracker)
+
+        else:
+            self.seams = []
+            for c in range(self.width):
+                self.naive_path(c, 0,[(c,0)], 10000)
+            best = min(self.seams, key=lambda n: n[1])
+        return best[0]
+    def remove_best_seam(self):
+        self.remove_seam(self.best_seam())
+obj = ResizeableImage('8x5.png')
+print(obj.best_seam(False))
--- ass5/resizeable_image.py
+++ ass5/resizeable_image.py
@@ -1,143 +1,49 @@
 import imagematrix
-import time
 
 class ResizeableImage(imagematrix.ImageMatrix):
 
-    best_path = []
-    best_seams = {}
+    def naive_path(self, w, h, path, e):
+        if h == self.height - 1:
+            self.seams.append((path,e))
+            return path
+
+        for c in range(max(0, w - 1), min(self.width, w + 2)):
+            energy = self.energy(c, (h + 1))
+            pixel = [(c, h + 1)]
+            self.naive_path(c, h + 1, path + pixel, e + energy)
+        
+        return
+
+        # if row == self.height - 1:
+        #     return [(col, row)]
+
     def best_seam(self, dp=True):
-        # calculate all energies
-        energy_memo = {}
-        for col in range(self.width):
-            for row in range(0, self.height):
-                energy_memo[(col, row)] = self.energy(col, row)
-        start_time = time.time()
+        # this list will have all the paths of all seams
+        self.seams = []
+
         if dp == False:
-            seam_list = []
-            for col in range(self.width):
-                seam = self.naive_alg(col, 0, energy_memo)
-                seam_list.append(seam)
-            min_seam = self.find_min(seam_list, energy_memo=energy_memo)
-            print("--- %s seconds ---" % (time.time() - start_time))
-            print(min_seam)
-        else:
-            seam_list = []
-            for col in range(self.width):
-                seam = self.dp_alg(col, 0, energy_memo)
-                seam_list.append(seam)
-            min_seam = self.find_min(seam_list, energy_memo=energy_memo)
-            print("--- %s seconds ---" % (time.time() - start_time))
-            print(min_seam)
+            col = 0
+            while col < self.width:
+                # third paramet is a list of tuple because it will allow me
+                # to manupulate it later on by appending paths to it
+                # fourth parameter is the energy at the current position
+                # which initializes at 0
+                self.naive_path(col, 0,[(col,0)], 10000)                    # for every pixel of first row, find a path
+                col += 1
+            
+            # this gets the seam with the lowest energy
+            # and its energy
+            best_path_with_lowest_energy = min(self.seams, key=lambda n: n[1])
+            best_path = best_path_with_lowest_energy[0]
+    
+        # returns seam with lowest energy
+        return best_path
+
+        if dp == True:
+            pass
 
     def remove_best_seam(self):
         self.remove_seam(self.best_seam())
-
-    def dp_alg(self, col, row, energy_memo):
-    # is col,row in best_seams[], return best seam tuples likst
-        if (col, row) in self.best_seams.keys():
-            return self.best_seams[(col, row)]
-        # if not, do naive call, before return statement, store result in lookup table
-        # base case, if we hit bottom return coords of pixel
-        if row == self.height - 1:
-            #return energy_memo[col, row]
-            return [(col, row)]
-        # recursaive case
-        # return pixel_energy(i,j) + min(seam energies of three pixels under)
-
-        # loop to check the next pixels
-        # for col in range(imagematrix.width):
-        # determine which pixels to check while staying in range of image
-        if col == 0:
-            seam_list = []
-            seam_list.append(self.dp_alg(col, row + 1, energy_memo))
-            seam_list.append(self.dp_alg(col + 1, row + 1, energy_memo))
-
-            min_seam = self.find_min(seam_list, energy_memo=energy_memo)
-            pos_tuple = (col, row)
-            min_seam.insert(0, pos_tuple)
-            self.best_seams[(col, row)] = min_seam
-            return min_seam
-            
-        elif col == self.width - 1:
-            seam_list = []
-            seam_list.append(self.dp_alg(col, row + 1, energy_memo))
-            seam_list.append(self.dp_alg(col - 1, row + 1, energy_memo))
-
-            min_seam = self.find_min(seam_list, energy_memo=energy_memo)
-            pos_tuple = (col, row)
-            min_seam.insert(0, pos_tuple)
-            self.best_seams[(col, row)] = min_seam
-            return min_seam
-
-        else:
-            seam_list = []
-            seam_list.append(self.dp_alg(col, row + 1, energy_memo))
-            seam_list.append(self.dp_alg(col - 1, row + 1, energy_memo))
-            seam_list.append(self.dp_alg(col + 1, row + 1, energy_memo))
-
-            min_seam = self.find_min(seam_list, energy_memo=energy_memo)
-            pos_tuple = (col, row)
-            min_seam.insert(0, pos_tuple)
-            self.best_seams[(col, row)] = min_seam
-            return min_seam
-
-
-    def naive_alg(self, col, row, energy_memo):
-    # is col,row in best_seams[], return best seam tuples likst
-        # if not, do naive call, before return statement, store result in lookup table
-        # base case, if we hit bottom return coords of pixel
-        if row == self.height - 1:
-            #return energy_memo[col, row]
-            return [(col, row)]
-
-        # loop to check the next pixels
-        # for col in range(imagematrix.width):
-        # determine which pixels to check while staying in range of image
-        if col == 0:
-            seam_list = []
-            seam_list.append(self.naive_alg(col, row + 1, energy_memo))
-            seam_list.append(self.naive_alg(col + 1, row + 1, energy_memo))
-
-            min_seam = self.find_min(seam_list, energy_memo=energy_memo)
-            pos_tuple = (col, row)
-            min_seam.insert(0, pos_tuple)
-            return min_seam
-
-        elif col == self.width - 1:
-            seam_list = []
-            seam_list.append(self.naive_alg(col, row + 1, energy_memo))
-            seam_list.append(self.naive_alg(col - 1, row + 1, energy_memo))
-
-            min_seam = self.find_min(seam_list, energy_memo=energy_memo)
-            pos_tuple = (col, row)
-            min_seam.insert(0, pos_tuple)
-            return min_seam
-
-        else:
-            seam_list = []
-            seam_list.append(self.naive_alg(col, row + 1, energy_memo))
-            seam_list.append(self.naive_alg(col - 1, row + 1, energy_memo))
-            seam_list.append(self.naive_alg(col + 1, row + 1, energy_memo))
-
-            min_seam = self.find_min(seam_list, energy_memo=energy_memo)
-            pos_tuple = (col, row)
-            min_seam.insert(0, pos_tuple)
-            return min_seam
-
-    def find_min(self, seam_list, energy_memo):
-        # return the list of tuples for the seam with the lowest energy
-        energy_list = []
-        energy = 0
-        for seam in seam_list:
-            # calculate energy of seam
-            energy = 0
-            for pixel in seam:
-                energy += energy_memo[pixel]
-            energy_list.append(energy)
-        min_index = energy_list.index(min(energy_list))
-        return seam_list[min_index]
-
-
-
-obj = ResizeableImage('8X8.png')
-obj.best_seam(dp=False)
+    
+obj = ResizeableImage('8x5.png')
+print(obj.best_seam(False))
